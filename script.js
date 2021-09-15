dd19ef40561d63d64353b49431cc15affd19af37
Nadia <nadiakrakowski@gmail.com>
2021-09-15 01:51:09 -0500

    updated themes, links, about

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const body = document.getElementById("body");
 
 theme.addEventListener("change", (e) => {
   let newTheme = e.target.value;
-  console.log(body);
+  console.log("before change:" + body.classList);
   body.removeAttribute("class");
   body.classList.add(newTheme);
+
+  console.log("after: " + body.classList);
 });
