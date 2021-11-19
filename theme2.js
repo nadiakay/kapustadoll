7cf992d171815359fafcc9ca1eaf905bfb14de05
Nadia Konieczny <nadiakrakowski@gmail.com>
2021-11-18 22:06:37 -0600

    completed gallery redesign & links

--- theme2.js
+++ theme2.js
@@ -3,12 +3,12 @@ const theme = document.getElementById("theme");
 if (localStorage.getItem("theme") !== null)
   setTheme(localStorage.getItem("theme"));
 else setTheme("theme-dark");
-//fix this: remove "theme" value & get value from localstorage if exists
 
-function setTheme(theme) {
-  localStorage.setItem("theme", theme);
+function setTheme(newTheme) {
+  localStorage.setItem("theme", newTheme);
   document.body.removeAttribute("class");
-  document.body.classList.add(theme);
+  document.body.classList.add(newTheme);
+  theme.value = newTheme;
 }
 
 theme.addEventListener("change", e => setTheme(e.target.value));
